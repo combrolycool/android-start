5a565bec35ac638fead32ca91325f68f451928df
Jorge de Jesus Torres Perez <gorgio_25@hotmail.com>
2018-09-19 09:28:18 -0400

    Initial commit

---
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/UserDetails.java
@@ -0,0 +1,7 @@
+package com.google.firebase.codelab.friendlychat;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
